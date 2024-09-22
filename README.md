c524a71f6603a5ba9054a0a033db5b8ed2336992
Bob Plotts <bob.plotts@dremio.com>
2024-09-22 14:52:29 -0400

    Changes for simpler requests.

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 
 This very simple program demonstrates connection pooling with the [Hikari Pool](https://github.com/brettwooldridge/HikariCP) connection pool library.
 
-This program requires two environment variables, DREMIO_USER, and DREMIO_PASSWORD.  If you cannot access Dremio's internal test machines, you'll need to modify the code and update the JDBC URL as well.
+This program requires two environment variables, DREMIO_USER, and DREMIO_PASSWORD.  If you cannot access Dremio's internal test machines, you'll need to modify the code and update the JDBC_URL as well.
 
-Currently this program is using the "legacy" Dremio JDBC Driver.  
+Currently this program is using the Apache JDBC SQL Arrow Flight JDBC Driver.  
 
 By default, the program creates 5 connections, currently this can be changed with --connections or -c
 
@@ -20,24 +20,14 @@ mvn clean package
 ```
 Run the program:
 ```shell
-java --illegal-access=warn -jar target/DremioConnectionPoolingExample-1.0-SNAPSHOT-jar-with-dependencies.jar -c 5 
+export _JAVA_OPTIONS="--illegal-access=warn --add-opens=java.base/java.nio=ALL-UNNAMED"
+java  -jar target/DremioConnectionPoolingExample-1.0-SNAPSHOT-jar-with-dependencies.jar -c 5 
 ```
 --connections or -c to set the number of JDBC connections.
 
 --threads or -t to set the number of worker threads.
 
 If you set the connections to be less than the number of threads, some threads will wait for db connections from the Hikari Pool.  
-For example, if you set `-c 2 -t 7` you might see output like this: 
-```shell
-2023-01-24 17:07:40.463 [INFO ] [Thread-3] DremioConnectionPoolingExample - 30000 records 40 records per ms.  pickup_datetime 2014-02-26 23:43:00.0
-2023-01-24 17:07:41.891 [INFO ] [Thread-0] DremioConnectionPoolingExample - 30000 records 13 records per ms.  pickup_datetime 2014-08-07 18:47:00.0
-2023-01-24 17:07:43.122 [INFO ] [Thread-3] DremioConnectionPoolingExample - 60000 records 17 records per ms.  pickup_datetime 2013-05-31 08:55:00.0
-2023-01-24 17:07:44.907 [INFO ] [Thread-0] DremioConnectionPoolingExample - 60000 records 11 records per ms.  pickup_datetime 2014-08-07 20:47:00.0
-2023-01-24 17:07:45.660 [INFO ] [Thread-3] DremioConnectionPoolingExample - 90000 records 15 records per ms.  pickup_datetime 2014-02-27 01:24:00.0
-2023-01-24 17:07:48.062 [INFO ] [Thread-0] DremioConnectionPoolingExample - 90000 records 10 records per ms.  pickup_datetime 2013-05-27 20:56:00.0
-2023-01-24 17:07:48.064 [INFO ] [Thread-3] DremioConnectionPoolingExample - 120000 records 14 records per ms.  pickup_datetime 2014-02-27 07:29:00.0
-```
-In which thread 0 and 3 are actively using the 2 connections, the other 5 threads are waiting. 
 
 With the default configuration a thread will only wait for 30 seconds, if a connection is not available, the thread will get an SQLException: 
 ```shell
@@ -49,10 +39,3 @@ java.sql.SQLTransientConnectionException: HikariPool-1 - Connection is not avail
 	at com.zaxxer.hikari.HikariDataSource.getConnection(HikariDataSource.java:100) ~[DremioConnectionPoolingExample-1.0-SNAPSHOT-jar-with-dependencies.jar:?]
 	at com.dremio.connectionpoolingexample.DremioConnectionPoolingExample$WorkerThread.run(DremioConnectionPoolingExample.java:110) ~[DremioConnectionPoolingExample-1.0-SNAPSHOT-jar-with-dependencies.jar:?]
 ```
-
-## Todo list - 
-- [x] test with the Arrow-Flight driver, in addition to the legacy driver. 
-- [ ] better record per second statistics to see how scalable this is. 
-- [x] add a logger; remove the System.out.println's
-- [ ] check into whether using Java 11 will remove the requirement for  `--illegal-access=warn`
-- 
--- src/main/java/com/dremio/connectionpoolingexample/DremioConnectionPoolingExample.java
+++ src/main/java/com/dremio/connectionpoolingexample/DremioConnectionPoolingExample.java
@@ -19,68 +19,66 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collections;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.*;
-import java.util.Collections;
-import java.util.ServiceLoader;
-
 public class DremioConnectionPoolingExample {
   private static final Logger LOG = LogManager.getLogger(DremioConnectionPoolingExample.class);
+
   @Parameter(names = {"--connections", "-c"})
-  int connections = 5;
+  int connections = 1;
+
+  @Parameter(names = {"--host", "-h"})
+  String host = "";
+
+  @Parameter(names = {"--port", "-p"})
+  int port = 0;
 
   @Parameter(names = {"--threads", "-t"})
-  int threads = connections;
+  int threads = 1;
 
+  // ArrayBlockingQueue<Record> theRecords = new ArrayBlockingQueue<>(100);
   public static void main(String... args) {
 
     DremioConnectionPoolingExample dcpe = new DremioConnectionPoolingExample();
 
-    //parse start args...
+    // parse start args...
     JCommander.newBuilder().addObject(dcpe).build().parse(args);
 
     dcpe.run();
   }
 
   void run() {
-    LOG.info("connections: {}", connections);
 
-    final String jdbcUrl = "jdbc:dremio:direct=automaster.drem.io:31010;schema=Samples.samples.dremio.com;";
-    final String arrowFlightURL = "jdbc:arrow-flight-sql://automaster.drem.io:32010/?useEncryption=false&schema=Samples.samples.dremio.com;";
-
+    final String arrowFlightURL =
+        String.format("jdbc:arrow-flight-sql://%s:%d/?useEncryption=false", host, port);
+    LOG.info(arrowFlightURL);
 
     // just to verify - print the available JDBC Drivers.
-    LOG.info("Currently Registered JDBC drivers:");
+    LOG.info("Registered JDBC drivers:");
     try {
-      Collections.list(DriverManager.getDrivers()).forEach(driver -> {
-        LOG.info("Driver class {} (version {}.{})", driver.getClass().getName(), driver.getMajorVersion(), driver.getMinorVersion());
-      });
+      Collections.list(DriverManager.getDrivers())
+          .forEach(
+              driver ->
+                  LOG.info(
+                      "Driver class {} (version {}.{})",
+                      driver.getClass().getName(),
+                      driver.getMajorVersion(),
+                      driver.getMinorVersion()));
     } catch (Exception ex) {
       LOG.error("Exception: {}", ex.getMessage(), ex);
       System.exit(4);
     }
 
-    try {
-      Class.forName("org.apache.arrow.driver.jdbc.ArrowFlightJdbcDriver");
-      Class.forName("org.postgresql.Driver");
-    } catch (ClassNotFoundException ex) {
-      LOG.error("Exception: {}", ex.getMessage(), ex);
-      System.exit(4);
-    }
-
-    ServiceLoader<Driver> loadedDrivers = ServiceLoader.load(Driver.class);
-    for (Driver driver : loadedDrivers) {
-      LOG.error("second try. Driver class {} (version {}.{})", driver.getClass().getName(), driver.getMajorVersion(), driver.getMinorVersion());
-    }
-
-    // create a connection pool.
     HikariConfig config = new HikariConfig();
 
-    // switch JDBC connection string as needed.
     config.setJdbcUrl(arrowFlightURL);
-//    config.setJdbcUrl(jdbcUrl);
 
     config.setUsername(System.getenv("DREMIO_USER"));
     config.setPassword(System.getenv("DREMIO_PASSWORD"));
@@ -98,32 +96,37 @@ public class DremioConnectionPoolingExample {
     HikariDataSource hikariPool;
 
     WorkerThread(HikariDataSource hikariPool) {
-      LOG.info("create WorkerThread");
-
       this.hikariPool = hikariPool;
     }
 
+    // invoked on thread.start()
     public void run() {
-      LOG.info("start WorkerThread");
 
       try {
         Connection conn = hikariPool.getConnection();
         Statement stmt = conn.createStatement();
         String dataSource = "Samples.\"samples.dremio.com\".\"NYC-taxi-trips\"";
-        final String sql = String.format("SELECT * FROM %s", dataSource);
-        ResultSet rs = stmt.executeQuery(sql);
+        final String sql = String.format("SELECT * FROM %s LIMIT 10", dataSource);
 
-        int i = 0;
-        long startTime = System.currentTimeMillis();
-        while (rs.next()) {
-          i++;
-          if (i % 30000 == 0) {
-            LOG.info("{} records {} records per ms.  pickup_datetime {}", i, i / (System.currentTimeMillis() - startTime), rs.getString("pickup_datetime"));
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 10_000_000; i++) {
+          try (ResultSet rs = stmt.executeQuery(sql)) {
+
+            //            String cat = conn.getCatalog();
+            //            DatabaseMetaData dbmd = conn.getMetaData();
+            while (rs.next()) {
+              rs.getString(1);
+            }
+            long end = System.currentTimeMillis();
+            if (i % 1000 == 0) {
+              System.out.println(
+                  String.format(
+                      "%s iteration: %d elapsed: %d",
+                      Thread.currentThread().getName(), i, end - start));
+            }
           }
         }
-        LOG.info("{} read {} records in {} ms.", Thread.currentThread().getName(), i, System.currentTimeMillis() - startTime);
 
-        rs.close();
         stmt.close();
         conn.close();
 
